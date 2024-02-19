4031020188647f2ef6bc0e762ba7373e12c13633
Dan Peacock <32961042+dan-peacock@users.noreply.github.com>
2024-02-19 23:51:14 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "vsphere" { 
+  user                 = var.username
+  password             = var.password
+  vsphere_server       = var.ip
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "datacenter" {
+  name = "dc1"
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "192.168.0.60/Resources"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "datastore1"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "test-TF"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = 1
+  guest_id         = "other3xLinux64Guest"
+  wait_for_guest_net_timeout = 0
+  # wait_for_guest_net_routable = false
+  memory           = 1024
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+    disk {
+    label = "test"
+    size  = "1024"
+  }
+}
