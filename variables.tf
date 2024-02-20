e94964fc5a890033e6736518ab3eeb0383333b7c
Dan Peacock <32961042+dan-peacock@users.noreply.github.com>
2024-02-20 20:05:35 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,17 @@ variable cloud {
   type        = string
   default     = "No"
 }
+
+variable username {
+  description = "vSphere administrator username"
+  type        = string
+  default     = "administrator@vsphere.local"
+}
+variable password {
+  description = "vSphere administrator password"
+  type        = string
+}
+variable ip {
+  description = "IP or FQDN of vSphere server"
+  type        = string
+}
