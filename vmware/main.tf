29b7fb96de6f95bcd1d99615322665dc1ed5ad88
Dan Peacock <32961042+dan-peacock@users.noreply.github.com>
2024-02-20 20:03:48 +0000

    Update main.tf

--- vmware/main.tf
+++ vmware/main.tf
@@ -1,10 +1,3 @@
-provider "vsphere" { 
-  user                 = var.username
-  password             = var.password
-  vsphere_server       = var.ip
-  allow_unverified_ssl = true
-}
-
 data "vsphere_datacenter" "datacenter" {
   name = "dc1"
 }
