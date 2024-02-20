07fcbef35a9b1b0a5ccc11554e7bc3538a8a78d5
Dan Peacock <32961042+dan-peacock@users.noreply.github.com>
2024-02-20 20:05:42 +0000

    Update variables.tf

--- vmware/variables.tf
+++ vmware/variables.tf
@@ -1,13 +1,1 @@
-variable username {
-  description = "vSphere administrator username"
-  type        = string
-  default     = "administrator@vsphere.local"
-}
-variable password {
-  description = "vSphere administrator password"
-  type        = string
-}
-variable ip {
-  description = "IP or FQDN of vSphere server"
-  type        = string
-}
+
