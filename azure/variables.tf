7cd8193e3f8e5c5858301ebbd46e725906d02ac8
Dan Peacock <32961042+dan-peacock@users.noreply.github.com>
2024-02-20 22:25:28 +0000

    Update variables.tf

--- azure/variables.tf
+++ azure/variables.tf
@@ -1,1 +1,30 @@
+variable "prefix" {
+  default = "dan-peacock"
+}
 
+variable "vm_size" {
+    description = "Value of the vm_size for the azure VM"
+    type = string
+    default = "Standard_DS1_v2"
+}
+
+variable "default_password" {
+    description = "inital password for VM"
+    type = string
+    default = "Password1234!"
+}
+
+variable "tags" {
+  description = "Additional resource tags"
+  type        = string
+}
+
+variable "subscription_ID" {
+    description = "SP AppID"
+    type = string
+}
+
+variable "tenant_ID" {
+    description = "SP AppID"
+    type = string
+}
