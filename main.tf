123978100aba3aa82809ab38dc5a1b96887c24b2
Dan Peacock <32961042+dan-peacock@users.noreply.github.com>
2024-02-20 19:59:19 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "vmware" {
   source = "./vmware"
-  count  = var.cloud == "yes" ? 1 : 0
+  count  = var.cloud == "Yes" ? 1 : 0
 }
 
 module "azure" {
   source = "./azure"
-  count  = var.cloud == "no" ? 1 : 0
+  count  = var.cloud == "No" ? 1 : 0
 }
