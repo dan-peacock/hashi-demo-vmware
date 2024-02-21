c86d3c4b527a2f6a058917ec973783eba5b5086e
Dan Peacock <32961042+dan-peacock@users.noreply.github.com>
2024-02-21 00:02:31 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -4,3 +4,9 @@ provider vsphere {
   vsphere_server       = var.ip
   allow_unverified_ssl = true
 }
+
+provider "azurerm" {
+  features {}
+  subscription_id   = var.subscription_ID
+  tenant_id         = var.tenant_ID
+}
